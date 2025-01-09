b38c3938c6fd41040cbfce3b1c2620a2e91e120d
cinema-talkies-app2-io <cinema.talkies.app2.io@gmail.com>
2025-01-09 13:26:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -79,7 +79,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://empirical-tortoise-cinematalkies-adf94198.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
